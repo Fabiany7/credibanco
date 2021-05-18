57e9f7af12355eaedac5ae8000cb8caf335ce4cb
Fabiany7 <fabianymadrigalsalazar@gmail.com>
2021-05-18 12:01:15 -0500

    primer commit

---
+++ src/test/java/com/credibanco/nexos/credibanco/CredibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credibanco.nexos.credibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
